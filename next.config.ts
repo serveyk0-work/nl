777f92ae1dea8e2a30ca793cac870e423d401212
serveyk0-work <serveyko.work@gmail.com>
2025-08-10 10:35:13 +0300

    123

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-  output: "export", // Генерує статичний сайт
-  basePath: isProd ? "/nl" : "", // назва твого репозиторію
-  assetPrefix: isProd ? "/nl/" : "",
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: "/nl",
+  basePath: "/nl",
+  output: "export",
 };
 
 export default nextConfig;
